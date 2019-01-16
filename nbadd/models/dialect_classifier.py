1b300e2b071b3e799d52ed840739d3d9f2842388
Andrew Moore <andrew.p.moore94@gmail.com>
2019-01-16 00:02:28 +0000

    All new results with attention

--- nbadd/models/dialect_classifier.py
+++ nbadd/models/dialect_classifier.py
@@ -2,6 +2,7 @@ from typing import Optional, Dict
 
 from allennlp.data import Vocabulary
 from allennlp.modules import FeedForward, Seq2VecEncoder, TextFieldEmbedder
+from allennlp.modules import InputVariationalDropout
 from allennlp.models.model import Model
 from allennlp.nn import InitializerApplicator, RegularizerApplicator
 from allennlp.nn import util
@@ -9,6 +10,7 @@ from allennlp.training.metrics import CategoricalAccuracy, F1Measure
 import numpy as np
 import torch
 import torch.nn.functional as F
+from torch.nn import Linear, Dropout
 
 
 @Model.register("dialect_classifier")
@@ -17,19 +19,22 @@ class DialectClassifier(Model):
                  vocab: Vocabulary,
                  text_field_embedder: TextFieldEmbedder,
                  text_encoder: Seq2VecEncoder,
-                 classifier_feedforward: FeedForward,
-                 dropout: Optional[float] = None,
+                 classifier_feedforward: Optional[FeedForward] = None,
+                 dropout: Optional[float] = 0.0,
                  initializer: InitializerApplicator = InitializerApplicator(),
                  regularizer: Optional[RegularizerApplicator] = None) -> None:
         super().__init__(vocab, regularizer)
-        if dropout:
-            self.dropout = torch.nn.Dropout(dropout)
-        else:
-            self.dropout = None
+        self._naive_dropout = Dropout(dropout)
+        self._variational_dropout = InputVariationalDropout(dropout)
+
         self.text_field_embedder = text_field_embedder
         self.num_classes = self.vocab.get_vocab_size("labels")
         self.text_encoder = text_encoder
         self.classifier_feedforward = classifier_feedforward
+        output_dim = text_encoder.get_output_dim()
+        if classifier_feedforward:
+            output_dim = classifier_feedforward.get_output_dim()
+        self.label_projection = Linear(output_dim, self.num_classes)
         self.metrics = {
                 "accuracy": CategoricalAccuracy()
         }
@@ -39,14 +44,17 @@ class DialectClassifier(Model):
     def forward(self,
                 text: Dict[str, torch.LongTensor],
                 label: torch.LongTensor = None) -> Dict[str, torch.Tensor]:
+        # Embed text
         embedded_text = self.text_field_embedder(text)
-        if self.dropout:
-            embedded_text = self.dropout(embedded_text)
+        embedded_text = self._variational_dropout(embedded_text)
         text_mask = util.get_text_field_mask(text)
+        # Encode text
         encoded_text = self.text_encoder(embedded_text, text_mask)
-        if self.dropout:
-            encoded_text = self.dropout(encoded_text)
-        logits = self.classifier_feedforward(encoded_text)
+        encoded_text = self._naive_dropout(encoded_text)
+
+        if self.classifier_feedforward:
+            encoded_text = self.classifier_feedforward(encoded_text)
+        logits = self.label_projection(encoded_text)
         class_probabilities = F.softmax(logits, dim=-1)
 
         output_dict = {"class_probabilities": class_probabilities}
